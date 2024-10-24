c753df8e33839d56cae9836543c3d0ef9bdda4f5
Vasiok11 <pascari.vasile27@gmail.com>
2024-10-24 21:28:50 +0300

    Added Display Class for comparison of displays. Comparisons displayed in a menu. Task 1 complete

---
+++ src/Lab1/task1/Display.java
@@ -0,0 +1,62 @@
+package Lab1.task1;
+
+public class Display {
+    private int width, height;
+    private float ppi;
+    private String model;
+
+    public Display(int width, int height, float ppi, String model) {
+        this.width = width;
+        this.height = height;
+        this.ppi = ppi;
+        this.model = model;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setPpi(float ppi) {
+        this.ppi = ppi;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public float getPpi() {
+        return ppi;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    void compareSize(Display m) {
+        String s = (this.width * this.height) > (m.getWidth() * m.getHeight()) ? this.model + " is bigger\n" : (this.width * this.height) < (m.getWidth() * m.getHeight()) ? m.getModel() + " is bigger" : "The displays are equal";
+        System.out.println(s);
+    }
+
+    void compareSharpness(Display m) {
+        String s = (this.ppi) > (m.getPpi()) ? this.model + " is sharper\n" : (this.ppi) < (m.getPpi()) ? m.getModel() + " is sharper" : "The displays have equal sharpness";
+        System.out.println(s);
+    }
+
+    void compareWithMonitor(Display m) {
+        compareSize(m);
+        compareSharpness(m);
+
+    }
+}
