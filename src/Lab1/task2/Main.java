1ffa4faf6c73186c834ade3d7204f8c7c8e1fdcb
Vasiok11 <pascari.vasile27@gmail.com>
2024-10-27 21:59:20 +0200

    Added the possiblity to add multiple files in args and print the TextData of each one. Task 4 complete.

--- src/Lab1/task2/Main.java
+++ src/Lab1/task2/Main.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = args[0];
-        try {
-            String text = FileReader.fileToString(filePath);
-            TextData textData = new TextData(filePath, text);
-            System.out.println(textData);
-        } catch (IOException e) {
-            System.err.println("Couldn't read file: " + e.getMessage());
+        for (String filePath : args) {
+            try {
+                String text = FileReader.fileToString(filePath);
+                TextData textData = new TextData(filePath, text);
+                System.out.println(textData);
+            } catch (IOException e) {
+                System.err.println("Couldn't read file: " + e.getMessage());
+            }
         }
     }
 }
